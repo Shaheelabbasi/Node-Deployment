e769caab3631229fa73b1a7d1534efe362ae4797
Shaheel Abbasi <139051885+Shaheelabbasi@users.noreply.github.com>
2025-12-17 23:25:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get('/hello', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
